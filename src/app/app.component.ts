eed5eabc32f69e56f0e146ee95b6c08c774ec1a6
jing-gu <jinggu.dev@gmail.com>
2025-07-11 23:17:24 -0400

    add github action for auto deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { JsonPipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [JsonPipe, FormsModule, InputTextModule],
+  imports: [FormsModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
